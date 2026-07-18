52b8205126393ac7de2449b9ab18a7245f0ed3de
Austin Pinto <austinsitoo342@gmail.com>
2026-07-18 02:07:49 +0200

    Corregido nombre politica-privacidad

--- src/routes/politica-privacidad.tsx
+++ src/routes/politica-privacidad.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/pilitica-privacidad")({
+export const Route = createFileRoute()({
   component: PoliticaPrivacidad,
 });
 
